fa20e7c7dd54163d47c04d0bb23f54aad8aba8b9
JosePinzon <josefpinzon@hotmail.com>
2025-01-31 22:05:55 -0500

    Se crea la funcion actualizarListaAmigos

--- app.js
+++ app.js
@@ -7,13 +7,33 @@ let amigos = [];
 //Agregar amigos al array, validar campo vacio y limpiar campo.
 function agregarAmigo() {
     let nombreAmigoInsertado = document.getElementById('amigo').value;
-    if ( nombreAmigoInsertado == "") {
-       
-        alert("Ingrese un nombre, es obligatorio");
+    if ( nombreAmigoInsertado) {
+
+        amigos.push(nombreAmigoInsertado); //Agrega el nombre a la ultima posicion de amigos[]
+        document.getElementById('amigo').value = ''; //Limpia el cajon
+        actualizarListaAmigos(); //Agrega los nombres a la lista y la muestra en pantalla
 
     } else {
-        amigos.push(nombreAmigoInsertado);
-        document.getElementById('amigo').value = '';
+
+        alert("Ingrese un nombre, es obligatorio");
+
+    };
+
+};
+
+//Agregar los nombre a la lista y limpiar la lista.
+
+function actualizarListaAmigos() {
+    
+    let listaEnPantalla = document.getElementById('listaAmigos');
+    listaEnPantalla.innerHTML = ""; //Limpia Lista
+
+    for (let x = 0; x < amigos.length; x++){
+       
+        let nuevoElementoLista = document.createElement('li'); //Crea Nuevo elemento en la lista
+        nuevoElementoLista.textContent = amigos [x]; //Agrega el contenido
+        listaEnPantalla.appendChild(nuevoElementoLista); //Agrega el li a la lista
+
     };
 
 };
