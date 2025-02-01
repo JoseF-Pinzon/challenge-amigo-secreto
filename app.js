2b82ce497e416e632657d38099a4326eed1728f9
JosePinzon <josefpinzon@hotmail.com>
2025-01-31 21:12:30 -0500

    Se crea la función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+ 
+//Variables Globales
+
+let amigo = [];
+
+function agregarAmigo() {
+    let nombreAmigoInsertado = document.getElementById('amigo').value;
+    if ( nombreAmigoInsertado == "") {
+       
+        alert("Ingrese un nombre, es obligatorio");
+
+    } else {
+        amigo.push(nombreAmigoInsertado);
+        alert(amigo);
+        document.getElementById('amigo').value = '';
+    };
+
+};
